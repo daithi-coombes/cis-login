d03605ca29a07651650e0c57fadc0beacbcc2048
daithi <daithi@localhost.loc>
2012-07-23 02:04:26 +0200

    github basic auth working - place shortcode [CI Social Login] on wp page for login form

--- public_html/CISocialLoginClientGItHub.php
+++ public_html/CISocialLoginClientGItHub.php
@@ -6,8 +6,8 @@
 	<input type="hidden" name="cisocial-github-login" value="login"/>
 	
 	<ul>
-		<li><input type="text" name="user" id="user" value="webeire@gmail.com"/><label for="user">GitHub Username</label></li>
-		<li><input type="password" name="pswd" id="pswd" value="chanman77"/><label for="pswd">GitHub Password</label></li>
+		<li><input type="text" name="user" id="user"/><label for="user">GitHub Username</label></li>
+		<li><input type="password" name="pswd" id="pswd"/><label for="pswd">GitHub Password</label></li>
 		<li><input type="submit" value="Login with GitHub"/></li>
 	</ul>
 </form>
